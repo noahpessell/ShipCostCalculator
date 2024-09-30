79ad534a13dae03da3dd73b395be4f4be33e71f2
noahpessell <nwpessell@mail.com>
2024-09-29 23:25:09 -0400

    test commit

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,16 @@
 //return
 //end class
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        double itemPrice = 0;
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++)
+        {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
